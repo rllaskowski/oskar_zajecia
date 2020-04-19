056a04223b8ac7c5b80e00d1590ae7a48f5e1734
rllaskowski <robert.laskowski0@gmail.com>
2020-04-19 13:39:10 +0200

    update

---
+++ 19-04-2020/halloween2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int tab[100001];
+
+/*
+3 4 2 6  1
+3 7 9 15 16
+*/
+
+int main() { 
+    int t;
+    int n;
+    int p;
+    int k;
+    int s;
+    cin >> t;
+    cin >> n;
+    for(int i = 1;i<=n;i++){
+        cin >> tab[i];
+    }
+    for(int i = 0;i<t;i++);{
+        cin >> p;
+        cin >> k;
+        
+    }
+    
+    return 0;
+}
--- 19-04-2020/nawozy.cpp
+++ 19-04-2020/nawozy.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int tab[10]; //ile mamy kazdego z nawozow
+int nawozy[10]; //ile mamy kazdego z nawozow
 
 int main() {
     int n;
-    int l; 
+    int l;
+    int s = 0;
+    int g = 0;
     string a;
     cin >> n;
     cin >> l;
     cin >> a;
     
     for (int i = 1; i <= n; i++){
-        cin >> tab[i];
+        cin >> nawozy[i];
     }
     for (int i = 0;i < l;i++){
         if(a[i] != 'D'){
             char cyfra = a[i];
 
             int kategoria = cyfra-'0';
-        } 
+
+            if (nawozy[kategoria] > 0) {
+                nawozy[kategoria]--;
+            } else {
+                cout << "NIE";
+                return 0;
+            }
+        } else {
+            g++;
+        }
     }
-    
-    
+    for(int i = 0;i<n;i++){
+        s += nawozy[i];
+    }
+    if(s < g){
+        cout << "NIE";
+        return 0;
+    }
+    cout << "TAK";
     return 0;
 }
