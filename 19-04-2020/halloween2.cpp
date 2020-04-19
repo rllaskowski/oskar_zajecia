c7516e84e3b6115c3ffcca5100c9cf8fed7dd3a1
rllaskowski <robert.laskowski0@gmail.com>
2020-04-19 14:39:24 +0200

    update

--- 19-04-2020/halloween2.cpp
+++ 19-04-2020/halloween2.cpp
@@ -18,11 +18,13 @@ int main() {
     cin >> n;
     for(int i = 1;i<=n;i++){
         cin >> tab[i];
+        tab[i] += tab[i-1];
     }
     for(int i = 0;i<t;i++);{
         cin >> p;
         cin >> k;
         
+        cout << tab[k] - tab[p - 1] << endl;
     }
     
     return 0;
