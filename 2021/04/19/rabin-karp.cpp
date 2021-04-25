7356b5a6dd0935d25d0bc545f7a807c008dc8923
Robert Laskowski <robert.laskowski0@gmail.com>
2021-04-25 19:22:55 +0200

    update

--- 2021/04/19/rabin-karp.cpp
+++ 2021/04/19/rabin-karp.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 const int mod = 1000000009;
 
+
+long long hashSuf[1000000];
 long long pot[1000000]; 
 long long wyliczHash(string t) {
     long long hash = 0; 
     long long h = 0;
     for(int i = t.size()-1;i>=0;i--){
-       
         // i jest dlugoscia sufkisu
         // teraz do hasha dojdzie litera na indeksie?
         hash = (hash+(t[i] - 'a')*pot[h]) % mod;
@@ -18,14 +19,40 @@ long long wyliczHash(string t) {
     return hash;
 } 
 
+
+void hasheSufiksow(string t) {
+    hashSuf[t.size()] = 0;
+    int h = 0;
+    for(int i = t.size()-1;i>=0;i--){
+        hashSuf[i] = (t[i]*pot[h] + hashSuf[i+1])%mod;
+    }
+}
+
+long long zamienLadnie(long long w) {
+    return ((w % mod) + mod) % mod;
+}
+
 int main() {
+    string tekst, w;
+    cin >> tekst >> w; 
+   
     int n;
     pot[0] = 1;
     for(int i = 1;i<1000000;i++){
-        pot[i] = (pot[i-1] * 26) % mod;
+        pot[i] = (pot[i-1] * 27) % mod;
     }
+    hasheSufiksow(tekst);
+    long long wHash = wyliczHash(w);
+
     
-    cout << wyliczHash("asagjahvssadfdsfdsfdhkasd") << endl;
+    for(int i = 0;i<tekst.size() - w.size();i++){
+        long long l = zamienLadnie(hashSuf[i] - hashSuf[w.size() + i]);
+        long long r = zamienLadnie(wHash * pot[tekst.size() - (w.size()+i)]);
+        if(l == r){
+            cout << i << " " << i+w.size() << endl;
+        }
+    }
+    ///cout << wyliczHash("asagjahvssadfdsfdsfdhkasd") << endl;
 
     return 0;
 }
