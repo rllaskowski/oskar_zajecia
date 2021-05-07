dd131176e51054a032b7c44b6677c5ccc135d4dd
Robert Laskowski <robert.laskowski0@gmail.com>
2021-05-07 20:48:09 +0200

    update

--- 2021/05/07/drzewo_przedzialowe.cpp
+++ 2021/05/07/drzewo_przedzialowe.cpp
@@ -20,9 +20,20 @@ int query(int a, int b) {
     a += M; // przejsce z nr liscia na nr wierzcholka
     b += M; // przejsce z nr liscia na nr wierzcholka
 
-    int wyn = 0;
+    int wyn = max(tree[a], tree[b]);
 
-    
+    while(a / 2 == b / 2){
+        if(a%2 == 0){
+            wyn = max(wyn, tree[a+1]);
+        }
+        if(b%2 == 1){
+            wyn = max(wyn, tree[b-1]);
+        }
+        a/=2;
+        b/=2;
+    }
+
+    return wyn;
 }
 
 
