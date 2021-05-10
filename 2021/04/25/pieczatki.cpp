17819caf9d111be40cd94815d74ab86c100129c5
Robert Laskowski <robert.laskowski0@gmail.com>
2021-05-10 16:55:45 +0200

    update

---
+++ 2021/04/25/pieczatki.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+char obraz[101][101];
+vector<pair<int, int> >pieczatka[20];
+pair<int, int> rPieczatki[20];
+
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<m;j++){
+            cin >> obraz[i][j];
+        }
+    }
+    int h, a, b;
+    cin >> h
+
+    return 0;
+
+}
